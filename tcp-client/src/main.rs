d509429b9d16599da20b187f54cd50b76e2717dd
lyxxng <lisa.shan.young@gmail.com>
2025-05-09 15:28:03 -0700

    Remove path, make modules public, and add wasm compatibility

--- tcp-client/src/main.rs
+++ tcp-client/src/main.rs
@@ -1,11 +1,9 @@
-mod api;
-mod path;
-mod requests;
+pub mod api;
+pub mod path;
+pub mod requests;
 
 use api::{auth, sensor, session, session_sensor, session_sensor_data, user};
-use path::Path;
-use reqwest::Client;
-use tokio;
+use reqwest_wasm::Client;
 
 pub fn get_client() -> Client {
     let client = Client::new();
@@ -13,12 +11,6 @@ pub fn get_client() -> Client {
     return client;
 }
 
-pub fn get_path() -> Path {
-    let path = Path::new();
-
-    return path;
-}
-
-async fn main() {
+fn main() {
     println!("Starting client...");
 }
