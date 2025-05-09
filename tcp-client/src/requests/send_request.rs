13781d8f84e5e55ac5549205bfb154e18ef9dc7b
lyxxng <lisa.shan.young@gmail.com>
2025-05-09 15:27:17 -0700

    wasm compatibility

--- tcp-client/src/requests/send_request.rs
+++ tcp-client/src/requests/send_request.rs
@@ -1,5 +1,5 @@
-use reqwest::{
-    header::{HeaderMap, HeaderValue, CONTENT_TYPE, COOKIE},
+use reqwest_wasm::{
+    header::{HeaderMap, HeaderValue, CONTENT_LENGTH, CONTENT_TYPE, COOKIE},
     Client, Method,
 };
 use serde::Serialize;
@@ -8,21 +8,21 @@ use serde_json;
 pub async fn send_request<T>(
     client: &Client,
     method: &Method,
-    url: &str,
+    url: String,
     session_id: Option<&str>,
     body: Option<T>,
-) -> (reqwest::StatusCode, Option<serde_json::Value>, HeaderMap)
+) -> (reqwest_wasm::StatusCode, Option<serde_json::Value>, HeaderMap)
 where
     T: Serialize,
 {
     let mut request = client.request(method.clone(), url);
 
-    // Add CONTENT_TYPE header for POST and PATCH methods
+    // Add content-type header for POST and PATCH methods
     if *method == Method::POST || *method == Method::PATCH {
         request = request.header(CONTENT_TYPE, HeaderValue::from_static("application/json"));
     }
 
-    // Add session_id header if provided
+    // Add session_id in cookie header if provided
     if let Some(session_id) = session_id {
         match HeaderValue::from_str(&format!("session_id={}", session_id)) {
             Ok(value) => {
@@ -34,17 +34,27 @@ where
         }
     }
 
-    // Add request body if provided
+    // Check if there is a body to send in the request
     if let Some(body) = body {
+        // Serialize the body and get the length
+        let serialized_body = serde_json::to_vec(&body).unwrap();
+        let content_length = serialized_body.len();
+        
+        // Add content-length header
+        request = request.header(CONTENT_LENGTH, HeaderValue::from_str(&content_length.to_string()).unwrap());
+
+        // Add the json body to the request
         request = request.json(&body);
+    } else {
+        request = request.header(CONTENT_LENGTH, 0);
     }
 
-    // Send request and handle all errors
+    // Send request
     let res = match request.send().await {
         Ok(response) => response,
         Err(_) => {
             return (
-                reqwest::StatusCode::INTERNAL_SERVER_ERROR,
+                reqwest_wasm::StatusCode::INTERNAL_SERVER_ERROR,
                 None,
                 HeaderMap::new(),
             );
@@ -56,7 +66,7 @@ where
     let headers = res.headers().clone();
 
     // Receive json body if not No Content
-    let json = if status != reqwest::StatusCode::NO_CONTENT {
+    let json = if status != reqwest_wasm::StatusCode::NO_CONTENT {
         match res.json::<serde_json::Value>().await {
             Ok(body) => Some(body),
             Err(_) => None,
